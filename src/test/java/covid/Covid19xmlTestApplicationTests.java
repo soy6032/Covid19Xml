6f14d418e3dfd3ac5c4e4a0fa65077283f0fccf4
DESKTOP-BUMAKAD\82104 <thtto77@gmail.com>
2021-04-27 13:36:16 +0900

    applicationTest

---
+++ src/test/java/covid/Covid19xmlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19xmlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
