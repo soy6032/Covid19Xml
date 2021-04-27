22fb76e9545bb329d15a1eab611b12cc210d23c1
DESKTOP-BUMAKAD\82104 <thtto77@gmail.com>
2021-04-27 13:34:31 +0900

    applicationTest

---
+++ src/main/java/covid/Covid19xmlTestApplication.java
@@ -0,0 +1,13 @@
+package covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19xmlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19xmlTestApplication.class, args);
+	}
+
+}
